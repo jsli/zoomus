ebfe9cfafb4807aa35164a25e60fcc3a0639bab3
Manson Li <lijinsong@rouchi.com>
2018-04-18 17:57:56 +0800

    hide client v1
    Signed-off-by: Manson Li <lijinsong@rouchi.com>

--- zoomus/__init__.py
+++ zoomus/__init__.py
@@ -1,10 +1,9 @@
-"""Python wrapper around the Zoom.us REST API"""
+"""Python wrapper around the Zoom.us REST API v2"""
 
 from __future__ import (
     absolute_import,
     unicode_literals)
 
-from zoomus.client import ZoomClient
-from zoomus.client_v2 import ZoomClientV2
+from zoomus.client_v2 import ZoomClientV2 as ZoomClient
 
-__version__ = '0.1.7'
+__version__ = '0.2.1'
